979f86f4d1e8a4c5aba0fb070729f85fb3db2893
Kegan Pankratz <kegan.pankratz@adtran.com>
2022-12-05 11:42:41 -0800

    days 4 and 5 complete

---
+++ 2022/1204/program-2.rb
@@ -0,0 +1,15 @@
+f = File.new('input.txt', 'r')
+count = 0
+f.readlines.each do |line|
+  group1, group2 = line.split(',')
+  group1x, group1y = group1.split('-')
+  group2x, group2y = group2.split('-')
+  group1Sections = (group1x.to_i..group1y.to_i).to_a
+  group2Sections = (group2x.to_i..group2y.to_i).to_a
+  intersect = group1Sections.intersection(group2Sections)
+  if !intersect.empty?
+    count += 1
+  end
+end
+
+puts count
---
+++ 2022/1205/program-2.rb
@@ -0,0 +1,58 @@
+READ_TYPE = {
+  containers: 0,
+  instructions: 1
+}
+
+def clean_container_item(raw)
+  if raw.match?(/\w/)
+    raw.sub!('[','')
+    raw.sub!(']','')
+  else
+    # for debuging purposes
+    # raw = '  '
+    raw = nil
+  end
+end
+
+reading = READ_TYPE[:containers]
+container_list = []
+instructions_list = []
+f = File.new('input.txt', 'r')
+f.readlines.each do |line|
+  if line == "\n"
+    # switch reading method
+    reading = READ_TYPE[:instructions]
+    next
+  end
+
+  if reading == READ_TYPE[:containers]
+    # get individual containers on a row
+    row_array = line.scan(/(.{3} )|(.{3})/)
+      .map {|i| i.compact}
+      .flatten
+      .map {|i| clean_container_item(i)}
+    container_list.push(row_array) 
+  elsif reading == READ_TYPE[:instructions]
+    # clean instructions to just the numbers
+    clean_instructions = line.split(/\D/)
+      .delete_if {|i| i == ''}
+      .map {|i| i.to_i}
+    instructions_list.push(clean_instructions)
+  end
+
+end
+
+t_list = container_list.transpose
+t_list.map {|i| i.compact!}
+instructions_list.each do |instruction|
+  t_list.each {|i| puts "#{i}"}
+  count, from, to = instruction
+  puts "move #{count} from #{from} to #{to}"
+  items = t_list[from - 1].shift(count)
+  t_list[to - 1] = items.concat(t_list[to - 1])
+end
+
+puts "Containers"
+container_list.each {|i| puts i.join}
+t_list.each {|i| puts i.join}
+#instructions_list.each {|x| puts x.join}
