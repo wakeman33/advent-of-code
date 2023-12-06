9b94dab10f8d955694f3ca5d647ca54370b89c2c
Kegan Pankratz <kegan.pankratz@adtran.com>
2023-12-05 18:36:21 -0800

    attempt on part 2

--- 2023/py/1205/p5-b.py
+++ 2023/py/1205/p5-b.py
@@ -4,31 +4,129 @@ day = 5
 year = 2023
 
 
-def check_seed(seed, map_values):
-    print(map_values)
-    next_val = seed
-    for des, src, range_number in map_values:
-        print(f"src: {src}, des: {des}, range: {range_number}")
-        print(seed)
+def check_seed(seed_ranges, map_values):
+    print()
+    print()
+    new_seed_ranges = []
+    exit_flag = False
 
-        if seed >= src and seed < src + range_number:
-            diff = seed - src
-            print(f"DIFF: {diff}")
-            next_val = des + diff
-            break
-    
-    print(f"Next Val: {next_val}")
-    return next_val
+    while seed_ranges:
+        (seed_start, seed_range) = seed_ranges.pop()
+    # for seed_start, seed_range in seed_ranges:
+        print("Taking One Speed Range")
+        print(seed_ranges)
+        found = False
+        print(map_values)
+        for dest, src, range_number in map_values:
+            seed_end = seed_start + seed_range
+            src_end = src + range_number
+            print(f"SOURCE: {src} -> {src_end}")
+            print(f"SEED: {seed_start} -> {seed_end}")
+            if seed_start >= src and seed_start< src_end:
+                found = True
+                print("Seed in range")
+                offset = seed_start - src
+                end_offset = (seed_end) - (src_end)
+                print(f"OFFSET: {offset}")
+                print(f"END_OFFSET: {end_offset}")
+                if end_offset < 0:
+                    # whole range fits
+                    print("Whole seed range in range")
+                    new_seed_ranges.append((dest + offset, seed_range))
+                elif end_offset > 0:
+                    # partial range fits
+                    print("Whole seed range doesn't fit in range")
+                    add_range = (dest + offset, seed_range - end_offset)
+                    print("ADDING")
+                    print(add_range)
+                    new_seed_ranges.append(add_range)
 
-# file = open("example.txt", "r")
-# lines = list(map(lambda i: i.strip(), file.readlines()))
-data = get_data(day=day, year=year)
-lines = data.splitlines()
+
+                    print(f"OFFSET: {offset}")
+                    print(f"END OFFSET: {end_offset}")
+
+                    missed = (src_end, end_offset)
+                    print(f"MISSED: {missed}")
+                    seed_ranges.append(missed)
+                    print(seed_ranges)
+                    found = False
+                    exit()
+                    break
+                else:
+                    print("Fits Exactly ")
+                    new_seed_ranges.append((dest + offset, seed_range))
+            elif src >= seed_start and src < seed_end:
+                print("Range in seed range")
+                found = True
+                offset = src - seed_start
+                end_offset = (src_end) - (seed_end)
+                print(f"OFFSET: {offset}")
+                print(f"END OFFSET: {end_offset}")
+                if end_offset < 0:
+                    add_range = (dest, range_number)
+                    new_seed_ranges.append(add_range)
+                    print("Whole range in seed range")
+
+                    # Adding before range
+                    seed_range = offset
+                    print("Continuing From")
+                    print(f"SEED_RANGE: {seed_start} -> {seed_range}")
+
+                    # Adding after range
+                    missed_1 = (seed_end, seed_end - src_end)
+                    missed_2 = (seed_start, offset)
+                    print(f"ADD: {missed_1}")
+                    print(f"ADD: {missed_2}")
+                    seed_ranges.append(missed_1)
+                    seed_ranges.append(missed_2)
+                    print(seed_ranges)
+                    found = False
+                    exit()
+                    break
+                    
+                elif end_offset > 0:
+                    print("Whole range NOT in seed range")
+
+                    add_range = (dest, seed_range - offset)
+                    print(f"NEW RANGE: {add_range}")
+                    new_seed_ranges.append((dest, seed_range - offset))
+
+                    missed = (seed_start, offset)
+                    print(f"MISSED: {missed}")
+                    seed_ranges.append(missed)
+                    print(seed_ranges)
+
+                    print(new_seed_ranges)
+                    # exit_flag = True
+                    found = False
+                    exit_flag = True
+                    break
+                else:
+                    print("Fits Exactly ")
+                    new_seed_ranges.append((dest + offset, seed_range))
+
+            
+            
+        if not found:
+            new_seed_ranges.append((seed_start, seed_range))
+
+
+
+    print(f"Next Val: {new_seed_ranges}")
+    print()
+
+    if exit_flag:
+        exit()
+    return new_seed_ranges
+
+file = open("example.txt", "r")
+lines = list(map(lambda i: i.strip(), file.readlines()))
+# data = get_data(day=day, year=year)
+# lines = data.splitlines()
 
 seeds = lines.pop(0).split(":")[1].strip().split(" ")
-seeds = map(int, seeds)
-print(seeds)
-print()
+seeds = list(map(int, seeds))
+seeds = list(zip(*[iter(seeds)]*2))
 
 lines.pop(0)
 lines.pop(0)
@@ -46,19 +144,19 @@ for line in lines:
 chart.append(map_values)
 
 locations = []
-for seed in seeds:
-    value = seed
+for seed_range in seeds:
+    curr_seed_ranges = [seed_range]
     for map_values in chart:
-        value = check_seed(value, map_values)
+        curr_seed_ranges = check_seed(curr_seed_ranges, map_values)
         # print(map_values)
     
-    print(f"LOCATION: {value}")
-    locations.append(value)
+    locations += curr_seed_ranges
 
 sorted_answer = sorted(locations)
 print(sorted_answer)
-answer = sorted_answer[:1]
-print(answer)
+location, range_value= sorted_answer[:1][0]
 
-result = submit(answer[0], part="a", day=day, year=year)
+print(f"LOCATION: {location}")
+
+result = submit(location, part="b", day=day, year=year)
 print(result)
