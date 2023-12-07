ca7029db87c35c08a52c6a09b85be6abd08b99a6
Kegan Panratz <wakeman33@Kegans-MBP.lan>
2023-12-07 09:58:05 -0800

    part of the way through day 7 part a

--- 2023/py/1207/p7-a.py
+++ 2023/py/1207/p7-a.py
@@ -1,7 +1,7 @@
 from aocd import get_data, submit
-from functools import reduce
+from functools import reduce, cmp_to_key
 
-day = 2
+day = 7
 year = 2023
 
 file = open("example.txt", "r")
@@ -9,27 +9,44 @@ lines = list(map(lambda i: i.strip(), file.readlines()))
 # data = get_data(day=day, year=year)
 # lines = data.splitlines()
 
+card_heiarchy = {"A": 1, "K": 2, "Q": 3, "J": 4, "T": 5, "9": 6, "8": 7, "7": 8, "6": 9, "5": 10, "4": 11, "3": 12, "2": 13}
 list_of_hands = list(map(lambda line: line.strip().split(" "),lines))
-sorted_hands = []
+print(list_of_hands)
+sorted_hands = {}
 
+def custom_sort(hand_1):
+    print("Sorting")
+    print(hand)
 
-def number_of_unique_cards(hand_dict):
-    card_count = len(hand_dict)
-    if card_count == 1:
+    number = ""
+    for i in hand_1[0]:
+        number += str(card_heiarchy[i])
+    return int(number)
+
+def identify_hand_type(hand_dict):
+    card_type_count = len(hand_dict)
+    if card_type_count == 1:
+        return "five_of_a_kind"
         # Five of a kind
-    elif card_count == 2:
+    elif card_type_count == 2:
+        card, count = list(hand_dict.items())[0]
+        if count == 3 or count == 2:
+            return "full_house"
+        else:
+            return "four_of_a_kind"
         # Full House or Four of a kind
-    elif card_count == 3:
+    elif card_type_count == 3:
+        for card, count in hand_dict.items():
+            if count == 2:
+                return "two_pair"
+            elif count == 3:
+                return "three_of_a_kind"
         # Two pair or Three of a kind
-    elif card_count == 4:
+    elif card_type_count == 4:
+        return "one_pair"
         # One pair
     else:
-        # High card
-
-
-def sort_hand(hand_dict):
-
-
+        return "high_card"
 
 for hand, bid in list_of_hands:
     print(f"HAND: {hand}")
@@ -40,9 +57,44 @@ for hand, bid in list_of_hands:
             hand_dict[card] += 1
         else:
             hand_dict[card] = 1
-    
-    print(hand_dict)
+    hand_type = identify_hand_type(hand_dict)    
+    if hand_type not in sorted_hands:
+        sorted_hands[hand_type] = [(hand, bid)]
+    else:
+        sorted_hands[hand_type].append((hand, bid))
 
+print(sorted_hands)
+for hand_type, hand_list in sorted_hands.items():
+    print(hand_type)
+    print(hand_list)
+    new_sort = sorted(sorted_hands[hand_type], key=custom_sort)
+    print(new_sort)
+    sorted_hands[hand_type] = new_sort
 
-# result = submit(count, part="a", day=day, year=year)
-# print(result)
+print(sorted_hands)
+
+ranked_hand_tuple_list = []
+if "five_of_a_kind" in sorted_hands:
+    ranked_hand_tuple_list += sorted_hands["five_of_a_kind"]
+if "four_of_a_kind" in sorted_hands:
+    ranked_hand_tuple_list += sorted_hands["four_of_a_kind"]
+if "full_house" in sorted_hands:
+    ranked_hand_tuple_list += sorted_hands["full_house"]
+if "three_of_a_kind" in sorted_hands:
+    ranked_hand_tuple_list += sorted_hands["three_of_a_kind"]
+if "two_pair" in sorted_hands:
+    ranked_hand_tuple_list += sorted_hands["two_pair"]
+if "one_pair" in sorted_hands:
+    ranked_hand_tuple_list += sorted_hands["one_pair"]
+if "high_card" in sorted_hands:
+    ranked_hand_tuple_list += sorted_hands["high_card"]
+
+print(ranked_hand_tuple_list)
+
+sum = 0
+for rank, hand_tuple in enumerate(list(reversed(ranked_hand_tuple_list))):
+    sum += (rank + 1) * int(hand_tuple[1])
+
+print(sum)
+result = submit(sum , part="a", day=day, year=year)
+print(result)
